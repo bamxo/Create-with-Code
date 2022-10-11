858a9f1427fc71cf568f43a95e9cd287426a870e
bamxo <thegeneralbam@gmail.com>
2022-10-10 23:53:21 -0700

    add game mechanics
    -player controls
    -gravity to vehicle and obstacles

---
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // public variables
+    public float speed;
+    public float turnSpeed;
+    
+    //private variables
+    private float horizontalInput;
+    private float forwardInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // player input
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        
+        // we move the vehicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // we turn the vehicle
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+    }
+}
